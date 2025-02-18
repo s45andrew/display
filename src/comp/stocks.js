24a5c043b7b8e579195e4e917a9eff29beabcc76
s45andrew <s45andrew@googlemail.com>
2025-02-18 11:15:52 +0000

    Fix className typo and use environment variables in NFL component

--- src/comp/stocks.js
+++ src/comp/stocks.js
@@ -1,43 +1,38 @@
 // src/components/About.js
 import React from 'react';
-
 import '../comp/stocks.css';
+
 const Stock = () => {
   return  (
-  <div className='stocks'>
-            <h1>stock details Page</h1><br />
-            <div class="flex-container">
-              
-  <div class="flex-child magenta">
-  <div class="button-container">
-  <button class="button-54" role="button">b1</button>
-  <button class="button-54" role="button">b2</button>
-  <button class="button-54" role="button">b3</button>
-  <button class="button-54" role="button">b4</button>
-  <button class="button-54" role="button">b5</button>
-</div>
-
-  the left side
-  </div>
-  
-  <div class="flex-child green">
-  twitter news<br />
-                                    <div className='myTwitter'><br />br twitter
-                                          <br />
-                                    </div>
-  </div>
-  
-</div>
-            <div className='jooiner'>
-                   <div className='other'>the left side</div>
-                                 <div className='right'>
-                                    twitter news<br />
-                                    <div className='myTwitter'><br />br twitter
-                                          <br />
-                                    </div>
-                                  </div>
-            </div>
-            </div>
+    <div className='stocks'>
+      <h1>stock details Page</h1><br />
+      <div className="flex-container">
+        
+        <div className="flex-child magenta">
+          <div className="button-container">
+            <button className="button-54" role="button">b1</button>
+            <button className="button-54" role="button">b2</button>
+            <button className="button-54" role="button">b3</button>
+            <button className="button-54" role="button">b4</button>
+            <button className="button-54" role="button">b5</button>
+          </div>
+          the left side
+        </div>
+        
+        <div className="flex-child green">
+          twitter news<br />
+          <div className='myTwitter'><br />br twitter<br /></div>
+        </div>
+        
+      </div>
+      <div className='jooiner'>
+        <div className='other'>the left side</div>
+        <div className='right'>
+          twitter news<br />
+          <div className='myTwitter'><br />br twitter<br /></div>
+        </div>
+      </div>
+    </div>
   );
 };
 
